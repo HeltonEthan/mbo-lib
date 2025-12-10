0dd122e455aef4382ece512f2c46bb90e2225892
HeltonEthan <HeltonEthan07@gmail.com>
2025-12-10 18:20:26 -0500

    engine is fully equipped for streaming and is ready to make a market around

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -2,12 +2,14 @@ use mbo_engine;
 use color_eyre::eyre::Result;
 
 #[test]
-fn test_run() -> Result<()> {
+fn integration_test() -> Result<()> {
     let config0 = mbo_engine::Config::new(
         "C:/Users/helto/GLBX-20250915-NGKNUL4VBG".to_string(),
-        "2025-05-14".to_string(),
-        "2025-05-23".to_string(),
+        "2025-05-12".to_string(),
+        "2025-05-17".to_string(),
     )?;
+
+    println!("config: {:#?}", config0);
 
     mbo_engine::run(config0)?;
     
